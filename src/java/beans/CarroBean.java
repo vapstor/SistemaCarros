3bf4e3bbd89b41c0c24e0a0ee0da110ed7d27237
matheusalbertomth <matheusalbertomth@gmail.com>
2018-05-14 19:56:45 -0300

    Commit Inicial
    Erro Conexão BD

---
+++ src/java/beans/CarroBean.java
@@ -0,0 +1,27 @@
+package beans;
+
+import entidades.dao.CarroDAO;
+import entidades.Carro;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class CarroBean extends CrudBean<Carro, CarroDAO> {
+
+    private CarroDAO carroDAO;
+    
+    @Override
+    public CarroDAO getDao() {
+        if(carroDAO == null){
+            carroDAO = new CarroDAO();
+        }
+        return carroDAO;
+    }
+
+    @Override
+    public Carro criarNovaEntidade() {
+        return new Carro();
+    }
+
+}
---
+++ src/java/exception/ErroSistema.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class ErroSistema extends Exception {
+
+    public ErroSistema(String message) {
+        super(message);
+    }
+
+    public ErroSistema(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
